659c4b819647063cc0e3452293ea1062911b9328
gchikukwa <chikukwagerald@yahoo.com>
2018-06-29 16:59:08 -0500

    Add files via upload

--- Cycle 07/networkscope.py
+++ Cycle 07/networkscope.py
@@ -5,18 +5,22 @@ import multiprocessing
 import subprocess
 import os
 
+#find host IP address
+def get_host_ip():
+   
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+    s.close()
+    return ip
 
-def pinger(job_q, results_q):
-    """
-    Do Ping
-    :param job_q:
-    :param results_q:
-    :return:
-    """
+#ping ip addresses
+def ping_ips(job_queue, results_queue):
+    
     DEVNULL = open(os.devnull, 'w')
     while True:
 
-        ip = job_q.get()
+        ip = job_queue.get()
 
         if ip is None:
             break
@@ -24,56 +28,39 @@ def pinger(job_q, results_q):
         try:
             subprocess.check_call(['ping', '-c1', ip],
                                   stdout=DEVNULL)
-            results_q.put(ip)
+            results_queue.put(ip)
         except:
             pass
 
-
-def get_my_ip():
-    """
-    Find my IP address
-    :return:
-    """
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    ip = s.getsockname()[0]
-    s.close()
-    return ip
-
-
+# Map the network / return list of valid addresses 
 def map_network(pool_size=255):
-    """
-    Maps the network
-    :param pool_size: amount of parallel ping processes
-    :return: list of valid ip addresses
-    """
-
+    
     ip_list = list()
 
-    # get my IP and compose a base like 192.168.1.xxx
-    ip_parts = get_my_ip().split('.')
+    # get host IP, cconstruct it like 192.168.1.xxx
+    ip_parts = get_host_ip().split('.')
     base_ip = ip_parts[0] + '.' + ip_parts[1] + '.' + ip_parts[2] + '.'
 
-    # prepare the jobs queue
+    # queueing jobs
     jobs = multiprocessing.Queue()
     results = multiprocessing.Queue()
 
-    pool = [multiprocessing.Process(target=pinger, args=(jobs, results)) for i in range(pool_size)]
+    ip_pool = [multiprocessing.Process(target=ping_ips, args=(jobs, results)) for i in range(pool_size)]
 
-    for p in pool:
+    for p in ip_pool:
         p.start()
 
-    # cue hte ping processes
+    # ping processes range
     for i in range(1, 255):
         jobs.put(base_ip + '{0}'.format(i))
 
-    for p in pool:
+    for p in ip_pool:
         jobs.put(None)
 
-    for p in pool:
+    for p in ip_pool:
         p.join()
 
-    # collect he results
+    # return IP addess list 
     while not results.empty():
         ip = results.get()
         ip_list.append(ip)
